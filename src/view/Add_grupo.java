6cba22778f39dac4e56007962f3a29d2be886449
Ana Beatriz <norbertoana16@gmail.com>
2022-08-29 13:11:44 -0300

    Commit teste

--- src/view/Add_grupo.java
+++ src/view/Add_grupo.java
@@ -1,91 +1,97 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import model.*;
-import model_database.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import model.Grupo;
+import model_database.Database;
 
 public class Add_grupo extends JFrame implements ActionListener {
 
-    private final  JLabel labelNome = new JLabel("Nome: ");
-    private final JTextField nome = new JTextField();
-    private final  JLabel labelId = new JLabel("ID: ");
-    private final JTextField id = new JTextField();
-    private final  JLabel labelQtde = new JLabel("Quantidade de pessoas: ");
-    private final JTextField qtde_pessoas = new JTextField();
-    private final  JButton Button_cadastrar = new JButton("Cadastrar");
-    private final  JButton Button_voltar = new JButton("Voltar");
+	private final JLabel labelNome = new JLabel("Nome: ");
+	private final JTextField nome = new JTextField();
+	private final JLabel labelId = new JLabel("ID: ");
+	private final JTextField id = new JTextField();
+	private final JLabel labelQtde = new JLabel("Quantidade de pessoas: ");
+	private final JTextField qtde_pessoas = new JTextField();
+	private final JButton Button_cadastrar = new JButton("Cadastrar");
+	private final JButton Button_voltar = new JButton("Voltar");
 
-    public Add_grupo(){
-        super("Cadastrar grupo");
-        JLabel label_titulo = new JLabel("Cadastre as seguintes informações");
-        label_titulo.setFont(new Font(("Verdana"), Font.PLAIN, 20));
-        label_titulo.setBounds(90, 10, 400, 40);
+	public Add_grupo() {
+		super("Cadastrar grupo");
+		JLabel label_titulo = new JLabel("Cadastre as seguintes informações");
+		label_titulo.setFont(new Font(("Verdana"), Font.PLAIN, 20));
+		label_titulo.setBounds(90, 10, 400, 40);
 
+		labelNome.setBounds(50, 70, 420, 30);
+		nome.setBounds(100, 70, 400, 30);
 
-        labelNome.setBounds(50, 70, 420, 30);
-        nome.setBounds(100, 70, 400, 30);
+		labelId.setBounds(50, 110, 420, 30);
+		id.setBounds(80, 110, 420, 30);
 
-        labelId.setBounds(50, 110, 420, 30);
-        id.setBounds(80, 110, 420, 30);
+		labelQtde.setBounds(50, 150, 420, 30);
+		qtde_pessoas.setBounds(230, 150, 270, 30);
 
-        labelQtde.setBounds(50, 150, 420, 30);
-        qtde_pessoas.setBounds(230, 150, 270, 30);
+		Button_voltar.setBounds(70, 220, 180, 30);
+		Button_voltar.setBackground(new Color(108, 112, 139));
+		Button_voltar.setForeground(new Color(222, 222, 245));
+		Button_voltar.addActionListener(this);
 
-        Button_voltar.setBounds(70, 220, 180, 30);
-        Button_voltar.setBackground(new Color(108, 112, 139));
-        Button_voltar.setForeground(new Color(222, 222, 245));
-        Button_voltar.addActionListener(this);
+		Button_cadastrar.setBounds(260, 220, 180, 30);
+		Button_cadastrar.setBackground(new Color(108, 112, 139));
+		Button_cadastrar.setForeground(new Color(222, 222, 245));
+		Button_cadastrar.addActionListener(this);
 
-        Button_cadastrar.setBounds(260, 220, 180, 30);
-        Button_cadastrar.setBackground(new Color(108, 112, 139));
-        Button_cadastrar.setForeground(new Color(222, 222, 245));
-        Button_cadastrar.addActionListener(this);
+		setLayout(null);
+		add(label_titulo);
+		add(labelNome);
+		add(nome);
+		add(labelId);
+		add(id);
+		add(labelQtde);
+		add(qtde_pessoas);
+		add(Button_cadastrar);
+		add(Button_voltar);
 
-        setLayout(null);
-        add(label_titulo);
-        add(labelNome);
-        add(nome);
-        add(labelId);
-        add(id);
-        add(labelQtde);
-        add(qtde_pessoas);
-        add(Button_cadastrar);
-        add(Button_voltar);
+		setSize(520, 340);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		getContentPane().setBackground(new Color(255, 255, 255));
 
-        setSize(520, 340);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        setResizable(false);
-        setLocationRelativeTo(null);
-        getContentPane().setBackground(new Color(255, 255, 255));
+	}
 
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == Button_voltar) {
+			Menu menu = new Menu();
+			dispose();
+		} else if (e.getSource() == Button_cadastrar) {
+			String nomeString = nome.getText();
+			String idString = id.getText();
+			String qtde_pessoasString = qtde_pessoas.getText();
+			try {
+				int id = Integer.parseInt(idString);
+				int qtde_pessoas = Integer.parseInt(qtde_pessoasString);
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == Button_voltar) {
-            Menu menu = new Menu();
-            dispose();
-        }
-        else if(e.getSource() == Button_cadastrar){
-            String nomeString = nome.getText();
-            String idString = id.getText();
-            String qtde_pessoasString = qtde_pessoas.getText();
-            try {
-                int id = Integer.parseInt(idString);
-                int qtde_pessoas = Integer.parseInt(qtde_pessoasString);
+				Grupo novo_grupo = new Grupo(qtde_pessoas, id, nomeString);
+				Database.getGrupos().add(novo_grupo);
+				Database.aumentar_contador_grupos();
 
-                Grupo novo_grupo = new Grupo(qtde_pessoas, id, nomeString);
-                Database.getGrupos().add(novo_grupo);
-                Database.aumentar_contador_grupos();
-
-                JOptionPane.showMessageDialog(null, "Seu Cadastro foi salvo com sucesso", "Cadastro", JOptionPane.PLAIN_MESSAGE);
-            } catch(NumberFormatException exception){
-                JOptionPane.showMessageDialog(null, "Algo de errado nao esta certo", "Erro", JOptionPane.PLAIN_MESSAGE);
-            }
-        }
-    }
+				JOptionPane.showMessageDialog(null, "Seu Cadastro foi salvo com sucesso", "Cadastro",
+						JOptionPane.PLAIN_MESSAGE);
+			} catch (NumberFormatException exception) {
+				JOptionPane.showMessageDialog(null, "Algo de errado nao esta certo", "Erro", JOptionPane.PLAIN_MESSAGE);
+			}
+		}
+	}
 }
--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,98 +1,103 @@
 package view;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Menu extends JFrame implements ActionListener {
 
-    private  final JLabel label_titulo = new JLabel("Despesas entre amigos");
-    private  final JButton add_grupo = new JButton("Grupo");
-    private  final JButton add_usuario = new JButton("Pessoa");
-    private  final JButton add_compra = new JButton("Compra");
-    private  final JButton add_imovel = new JButton("Imovel");
-    private  final JButton mostrar_divida = new JButton("Mostrar Divida");
-    private  final JButton deletar_grupo = new JButton("Deletar Grupo");
-   
+	private final JLabel label_titulo = new JLabel("Despesas entre amigos");
+	private final JButton add_grupo = new JButton("Grupo");
+	private final JButton add_usuario = new JButton("Pessoaa");
+	private final JButton add_compra = new JButton("Compra");
+	private final JButton add_imovel = new JButton("Imovel");
+	private final JButton mostrar_divida = new JButton("Mostrar Divida");
+	private final JButton deletar_grupo = new JButton("Deletar Grupo");
 
-    public Menu() {
-        super("Divisão de Despesas entre amigos");
+	public Menu() {
+		super("Divisão de Despesas entre amigos");
 
-        label_titulo.setFont(new Font(("Verdana"), Font.PLAIN, 25));
-        label_titulo.setBounds(120, 10, 300, 40);
+		label_titulo.setFont(new Font(("Verdana"), Font.PLAIN, 25));
+		label_titulo.setBounds(120, 10, 300, 40);
 
-        add_grupo.setBounds(160, 70, 180, 30);
-        add_grupo.setBackground(new Color(108, 112, 139, 255));
-        add_grupo.setForeground(new Color(222, 222, 245));
-        add_grupo.addActionListener(this);
+		add_grupo.setBounds(160, 70, 180, 30);
+		add_grupo.setBackground(new Color(108, 112, 139, 255));
+		add_grupo.setForeground(new Color(222, 222, 245));
+		add_grupo.addActionListener(this);
 
-        add_usuario.setBounds(160, 110, 180, 30);
-        add_usuario.setBackground(new Color(108, 112, 139));
-        add_usuario.setForeground(new Color(222, 222, 245));
-        add_usuario.addActionListener(this);
+		add_usuario.setBounds(160, 110, 180, 30);
+		add_usuario.setBackground(new Color(108, 112, 139));
+		add_usuario.setForeground(new Color(222, 222, 245));
+		add_usuario.addActionListener(this);
 
-        add_compra.setBounds(160, 150, 180, 30);
-        add_compra.setBackground(new Color(108, 112, 139));
-        add_compra.setForeground(new Color(222, 222, 245));
-        add_compra.addActionListener(this);
+		add_compra.setBounds(160, 150, 180, 30);
+		add_compra.setBackground(new Color(108, 112, 139));
+		add_compra.setForeground(new Color(222, 222, 245));
+		add_compra.addActionListener(this);
 
-        add_imovel.setBounds(160, 190, 180, 30);
-        add_imovel.setBackground(new Color(108, 112, 139));
-        add_imovel.setForeground(new Color(222, 222, 245));
-        add_imovel.addActionListener(this);
+		add_imovel.setBounds(160, 190, 180, 30);
+		add_imovel.setBackground(new Color(108, 112, 139));
+		add_imovel.setForeground(new Color(222, 222, 245));
+		add_imovel.addActionListener(this);
 
-        mostrar_divida.setBounds(160, 230, 180, 30);
-        mostrar_divida.setBackground(new Color(108, 112, 139));
-        mostrar_divida.setForeground(new Color(222, 222, 245));
-        mostrar_divida.addActionListener(this);
+		mostrar_divida.setBounds(160, 230, 180, 30);
+		mostrar_divida.setBackground(new Color(108, 112, 139));
+		mostrar_divida.setForeground(new Color(222, 222, 245));
+		mostrar_divida.addActionListener(this);
 
-        deletar_grupo.setBounds(160, 270, 180, 30);
-        deletar_grupo.setBackground(new Color(108, 112, 139));
-        deletar_grupo.setForeground(new Color(222, 222, 245));
-        deletar_grupo.addActionListener(this);
+		deletar_grupo.setBounds(160, 270, 180, 30);
+		deletar_grupo.setBackground(new Color(108, 112, 139));
+		deletar_grupo.setForeground(new Color(222, 222, 245));
+		deletar_grupo.addActionListener(this);
 
-        setLayout(null);
+		setLayout(null);
 
-        add(label_titulo);
-        add(add_grupo);
-        add(add_usuario);
-        add(add_compra);
-        add(add_imovel);
-        add(mostrar_divida);
-        add(deletar_grupo);
+		add(label_titulo);
+		add(add_grupo);
+		add(add_usuario);
+		add(add_compra);
+		add(add_imovel);
+		add(mostrar_divida);
+		add(deletar_grupo);
 
-        setSize(520, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        setResizable(true); //Centraliza
-        setLocationRelativeTo(null);
-        getContentPane().setBackground(new Color(255, 255, 255));
+		setSize(520, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setResizable(true); // Centraliza
+		setLocationRelativeTo(null);
+		getContentPane().setBackground(new Color(255, 255, 255));
 
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == add_grupo) {
-            Add_grupo add_grupo = new Add_grupo();
-        }
-        if (e.getSource() == add_usuario) {
-            Add_usuario add_usuario = new Add_usuario();
-        }
-        if (e.getSource() == add_compra) {
-            Add_compra add_compra = new Add_compra();
-        }
-        if (e.getSource() == add_imovel) {
-            Add_imovel add_imovel = new Add_imovel();
-        }
-        if (e.getSource() == mostrar_divida) {
-            MostrarDividas mostrarDividas = new MostrarDividas();
-        }
-        if (e.getSource() == deletar_grupo) {
-            DeletarGrupo deletarGrupo = new DeletarGrupo();
-        }
-        dispose();
-    }
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-    }
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == add_grupo) {
+			Add_grupo add_grupo = new Add_grupo();
+		}
+		if (e.getSource() == add_usuario) {
+			Add_usuario add_usuario = new Add_usuario();
+		}
+		if (e.getSource() == add_compra) {
+			Add_compra add_compra = new Add_compra();
+		}
+		if (e.getSource() == add_imovel) {
+			Add_imovel add_imovel = new Add_imovel();
+		}
+		if (e.getSource() == mostrar_divida) {
+			MostrarDividas mostrarDividas = new MostrarDividas();
+		}
+		if (e.getSource() == deletar_grupo) {
+			DeletarGrupo deletarGrupo = new DeletarGrupo();
+		}
+		dispose();
+	}
+
+	public static void main(String[] args) {
+		Menu menu = new Menu();
+	}
 }
